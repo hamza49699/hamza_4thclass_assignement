cbdbc72c1668e568c5a0573f157c4516e623ee74
Hamza Javed <159588350+hamza49699@users.noreply.github.com>
2024-02-29 05:16:25 +0500

    Add files via upload

---
+++ datatypes.ts
@@ -0,0 +1,33 @@
+//Example 1 string
+let MyName : string = "Hamza"
+console.log(MyName)
+
+//Example 2 number
+let MyRollno : number = 16194
+console.log(MyRollno)
+
+//Example 3 boolean
+let mystatus :  boolean = true
+if(mystatus){
+    console.log("condiotion is true")
+}
+else{
+    console.log("condition is false")
+}
+
+//Example 4 null
+let name_1 : null = null
+console.log(null)
+
+//Example 5 undefined
+let un_defined : undefined;
+console.log(undefined)
+
+//Example 6 bigint
+let bigIntValue: bigint = 1234567890123456789012345678901234567890n;
+
+console.log(bigIntValue);
+
+//Example 7 Symbol
+const sym: symbol = Symbol('7th example');
+console.log(sym);
